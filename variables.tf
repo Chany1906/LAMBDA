2367d51c96462116992c1293f06f0baf14bd4b8c
Chany <chany.vargas@upeu.edu.pe>
2025-06-23 22:45:10 -0500

    cambio out y var

--- variables.tf
+++ variables.tf
@@ -6,12 +6,13 @@ variable "output_bucket_arn" {
     type = string 
 }
 
-variable "suffix" {
-  type        = string
-  description = "Sufijo aleatorio para recursos únicos"
-}
 
 variable "s3_bucket_name" {
   description = "Nombre del bucket S3"
+  type        = string
+}
+
+variable "suffix" {
+  description = "Sufijo para evitar duplicados"
   type        = string
 }
